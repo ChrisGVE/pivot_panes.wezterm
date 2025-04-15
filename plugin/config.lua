f95bcf65f5cef50c7d125067d56f7308ed1b4bd0
Chris <chris@example.com>
2025-04-15 20:19:57 +0200

    fix(config):removed the pseudo cache since it contradicts the logic

--- plugin/config.lua
+++ plugin/config.lua
@@ -57,9 +57,7 @@ M.config = nil
 ---@param user_config PivotConfig|nil
 ---@return PivotConfig
 function M.setup(user_config)
-	if M.config == nil then
-		M.config = lib.table.tbl_deep_extend("force", M.default_config, user_config or {})
-	end
+	M.config = lib.table.tbl_deep_extend("force", M.default_config, user_config or {})
 	return M.config
 end
 
